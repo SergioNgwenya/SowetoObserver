2de476d86fef9684fb45515050230e3c40609a2a
nkateko93 <nkatekondlovo304@gmail.com>
2018-10-11 10:35:20 +0200

    pull

--- client/src/components/Headline/Headlines.js
+++ client/src/components/Headline/Headlines.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import { connect } from 'react-redux';
 
 class Headlines extends React.Component {
   render() {
+    const {articles} = this.props;
     return (
-      <ListGroup flush>
-        <ListGroupItem tag="a" href="#"> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</ListGroupItem>
-        <ListGroupItem tag="a" href="#">Dapibus ac facilisis in Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam</ListGroupItem>
-        <ListGroupItem tag="a" href="#">Morbi leo risus uis autem vel eum iure reprehenderit qui </ListGroupItem>
-        <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
-        <ListGroupItem tag="a" href="#">Vestibulum at eros qui in ea voluptate velit esse quam nihil molestiae </ListGroupItem>
+      <ListGroup flush className="headlines" style={{height: 400,overflowY:'scroll'}}>
+      {articles?
+        articles.map((art, ind)=>{
+          return(
+                    <ListGroupItem tag="a" href="#">{art.title} </ListGroupItem>
+          )
+        })
+        :<div>loading</div>
+      }
       </ListGroup>
     );
   }
 }
 
-export default Headlines;
+function mapStateToProps(state) {
+  return {
+    articles: state.articles,
+  }
+}
+
+export default connect(mapStateToProps)(Headlines);
